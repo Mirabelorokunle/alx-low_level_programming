4a9c7aa658704d4e7b0c4e8d4312bbc1c2c3dc06
Mirabelorokunle <mirabelorokunle@gmail.com>
2023-06-17 00:36:14 +0100

    all

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char alpha[] = "abcdfghijklmnoprstuvwxyz";
+int mimi;
+for (mimi = 0; mimi < 24; mimi++)
+putchar(alpha[mimi]);
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int mimi;
+for (mimi = 48; mimi < 58; mimi++)
+putchar(mimi);
+putchar('\n');
+return (0);
+}
